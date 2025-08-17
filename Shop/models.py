609bd96f71b614468d1e8d4ca0ea3e4dcfae08f0
Alixon <132573114+Alixon2010@users.noreply.github.com>
2025-08-17 14:13:51 +0500

    fixed bug

--- Shop/models.py
+++ Shop/models.py
@@ -26,3 +26,6 @@ class Product(models.Model):
     description = models.TextField(validators=[validators.MinLengthValidator(10), validators.MaxLengthValidator(4000)])
     price = models.DecimalField(max_digits=12, decimal_places=2)
     stock = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.name
