c95fe51892cf7d224b3e022250a0885e68cddc64
Is-Narzulla <163721234+Is-Narzulla@users.noreply.github.com>
2025-08-26 00:14:03 +0500

    Order modeli uchun OrderSerializer va OrderRetrieve class lari qushildi

--- Shop/urls.py
+++ Shop/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from rest_framework.routers import DefaultRouter
 
 from Shop import views
+from Shop.views import OrderListView, OrderRetrieveView
 
 router = DefaultRouter()
 router.register('categories', views.CategoryViewSet)
@@ -17,10 +18,10 @@ urlpatterns = [
     path('to_card/', views.ToCardView.as_view()),
     path('remove_card/', views.RemoveCardView.as_view()),
     path('to_order/', views.ToOrderView.as_view()),
-    path('change_order_status/<str:order_id>/<str:stat>/', views.ChangeOrderStatus.as_view())
+    path('change_order_status/<str:order_id>/<str:stat>/', views.ChangeOrderStatus.as_view()),
 
     path("orders/",OrderListView.as_view()),
-    path("orders/<order_user_id:pk>/",OrderRetrieveView.as_view()),
+    path("orders/<uuid:pk>/",OrderRetrieveView.as_view()),
 
 ]
 
--- root/settings.py
+++ root/settings.py
@@ -21,7 +21,7 @@ load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY', default="fallback-secret-key")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
