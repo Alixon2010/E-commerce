16c848a14f6f143ecdfb05ea7d5bb18f30b83011
TT_FLICK <djamalxanovalixon8@gmail.com>
2025-08-29 18:58:35 +0500

    fix product stock

--- Shop/models.py
+++ Shop/models.py
@@ -38,7 +38,7 @@ class Product(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(validators=[validators.MinLengthValidator(10), validators.MaxLengthValidator(4000)])
     price = models.DecimalField(max_digits=12, decimal_places=2)
-    stock = models.IntegerField(default=0)
+    stock = models.PositiveIntegerField(default=1)
 
     def __str__(self):
         return self.name
